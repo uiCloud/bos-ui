943f77baec0c41834e3ca62e66dfddcc8d527fd7
qiubw <months.neighs-0h@icloud.com>
2023-04-19 17:30:06 +0800

    feat: replace code viewer component based on 'monaco-editor'

--- examples/src/views/CodeViewer.vue
+++ examples/src/views/CodeViewer.vue
@@ -1,19 +1,53 @@
 <template>
   <div class="code-viewer-view">
     <h1>This is an code viewer page</h1>
-    <code-viewer></code-viewer>
+    <b-upload
+        drag
+        action="#"
+        :file-list="fileList"
+        :on-change="handleChange"
+    ></b-upload>
+    <code-viewer :origin-file="originFile"></code-viewer>
   </div>
 </template>
 
 <script>
+import { bufferToText, getFileExtension, readBuffer } from '../../../src/util/file-util'
+
 export default {
   name: 'CodeViewerView',
   data() {
     return {
+      originFile: {},
+      fileList: []
     }
   },
-  watch: {
+  updated() {
+    console.info(this.fileList)
   },
+  methods: {
+    async previewFile(fileObject) {
+      try {
+        const arrayBuffer = await readBuffer(fileObject)
+        const text = await bufferToText(arrayBuffer)
+        const language = getFileExtension(fileObject.name)
+        this.originFile = {
+          fileName: fileObject.name,
+          rawFile: fileObject,
+          text,
+          language
+        }
+      } catch (e) {
+        console.error(e)
+      }
+    },
+    handleChange(file, fileList) {
+      this.originFile = file.originFile
+      console.info(file, fileList)
+      this.previewFile(this.originFile)
+    }
+  },
+  watch: {},
   components: {}
 }
 </script>
--- packages/code-viewer/src/code-viewer.vue
+++ packages/code-viewer/src/code-viewer.vue
@@ -1,62 +1,79 @@
 <template>
-  <div class='code-viewer' @click="init">
-    <pre>
-      <code class="language-html">
-        {{ code }}
-      </code>
-    </pre>
+  <div class='code-viewer'>
+    <div ref="codeContainer" class="monaco-editor">
+    </div>
   </div>
 </template>
 
 <script>
-// https://highlightjs.org/download/
-import hljs from './highlight.min'
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
 
 export default {
   name: 'CodeViewer',
-  props: {},
+  props: {
+    originFile: {}
+  },
   data() {
     return {
-      theme: 'vs-2015',
-      code: `import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { CssBaseline } from '@nextui-org/react';
-
-class MyDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return {
-      ...initialProps,
-      styles: React.Children.toArray([initialProps.styles])
-    };
-  }
-
-  render() {
-    return (
-      <Html lang="en">
-        <Head>{CssBaseline.flush()}</Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
-}
-
-export default MyDocument;
-`
+      monacoEditor: null
     }
   },
   mounted() {
-    // 123123
-    this.init()
+    this.$nextTick(() => {
+      this.initMonaco()
+    })
   },
   methods: {
-    init() {
-      hljs.highlightAll()
+    initMonaco() {
+      if (this.$refs.codeContainer) {
+        // 初始化编辑器，确保dom已经渲染
+        this.monacoEditor = monaco.editor.create(this.$refs.codeContainer, {
+          value: this.originFile.text, // 编辑器初始显示文字
+          language: 'javascript', // 语言javascript | json
+          theme: 'vs', // 官方自带三种主题vs, hc-black, or vs-dark
+          foldingStrategy: 'indentation', // 代码可分小段折叠
+          overviewRulerBorder: false, // 不要滚动条的边框
+          renderLineHighlight: 'gutter', // 当前行突出显示方式
+          contextmenu: true, // 启用上下文菜单
+          copyWithSyntaxHighlighting: false, // 是否应将语法突出显示复制到剪贴板中 即 当你复制到word中是否保持文字高亮颜色
+          cursorSmoothCaretAnimation: true, // 是否启用光标平滑插入动画  当你在快速输入文字的时候 光标是直接平滑的移动还是直接"闪现"到当前文字所处位置
+          lineNumbers: 'on', // 控制行号的出现on | off
+          scrollbar: { // 滚动条设置
+            verticalScrollbarSize: 4, // 竖滚动条
+            horizontalScrollbarSize: 6 // 横滚动条
+          },
+          readOnly: true, // 是否只读 Defaults to false | true
+          minimap: { // 关闭小地图
+            enabled: true
+          },
+          cursorStyle: 'line', // 光标样式
+          fontSize: 14, // 字体大小
+          tabSize: 2, // tab缩进长度
+          automaticLayout: true, // 自动布局
+          autoIndent: true, // 自动缩进
+          roundedSelection: true, // 选区是否有圆角
+          scrollBeyondLastLine: false // 是否可以滚到最后一行之后
+        })
+      }
     }
   },
-  watch: {}
+  watch: {
+    originFile: {
+      handler(newVal) {
+        console.info(newVal)
+        this.monacoEditor.setValue(newVal.text)
+        this.monacoEditor.updateOptions({
+          language: newVal.language
+        })
+        this.monacoEditor.focus()
+        this.monacoEditor.setPosition({
+          lineNumber: 0,
+          column: 0
+        })
+        // this.monacoEditor.setScrollPosition(0)
+        this.monacoEditor.revealLine(0)
+      }
+    }
+  }
 }
 </script>
